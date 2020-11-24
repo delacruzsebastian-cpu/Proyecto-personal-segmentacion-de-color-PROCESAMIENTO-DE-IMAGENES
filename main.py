cffae4a29a436b0d1458923d26fef8e551452659
delacruzsebastian-cpu <69725344+delacruzsebastian-cpu@users.noreply.github.com>
2020-11-24 09:51:09 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from Bandera import *
+
+
+if __name__ == '__main__':
+    image = Bandera('E:\Andy\Downloads\procesamiento image','flag5.png')
+    image.Colores()
+    image.Porcentaje()
+    image.Orientacion()
+
+
